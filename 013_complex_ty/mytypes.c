f767402bfbb808d6dbc5e9ad415a637167b63582
cx43 <cx43@duke.edu>
2024-01-25 12:05:04 -0500

    13

--- 013_complex_ty/mytypes.c
+++ 013_complex_ty/mytypes.c
@@ -4,10 +4,11 @@ struct _complex_num_t{
 	}; 
 typedef struct _complex_num_t complex_num_t;
 
-typedef enum _animal_t{
+enum _animal_t{
 	CAT,
        	FISH,
        	SNAKE,
        	DOG,
        	HORSE,
-	}; animal_t;
+	}; 
+typedef enum _animal_t animal_t;
