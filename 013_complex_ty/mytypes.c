7b7c7977dbde72c09d7481f92e8488ba1164fab9
cx43 <cx43@duke.edu>
2024-01-25 11:56:35 -0500

    013

---
+++ 013_complex_ty/mytypes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct _complex_num_t{
+	double real_part;
+	double imaginary_part;
+	}; 
+typedef struct _complex_num_t complex_num_t;
+
+typedef enum _animal_t{
+	CAT,
+       	FISH,
+       	SNAKE,
+       	DOG,
+       	HORSE
+	}; animal_t;
