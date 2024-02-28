b4660be2b67ac992d875ff282d36034bf4137a8a
cx43 <cx43@duke.edu>
2024-02-27 20:45:59 -0500

    057

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -5,4 +5,18 @@
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  size_t length = strlen(inputName) + strlen(".counts") + 1; // plus one for '\0'
+  
+  char *outputName = malloc(length * (sizeof(*outputName)));
+  if (outputName == NULL) {
+    fprintf(stderr, "Failed to allocate momery.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Generate new fileName
+  strcpy(outputName, inputName);
+  strcat(outputName, ".counts"); //连接两个字符串
+
+  return outputName;
+
 }
